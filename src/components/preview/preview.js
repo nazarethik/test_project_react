def94874da0479d32149e637085f4c141b099191
Nazarii <nazarella8@gmail.com>
2023-04-16 13:40:31 +0300

    Workable preview components with styles

--- src/components/preview/preview.js
+++ src/components/preview/preview.js
@@ -1,13 +1,34 @@
 import vegan from '../../img/tabs/vegy.jpg';
-import hamburger from'../../img/tabs/hamburger.jpg';
+import elite from'../../img/tabs/elite.jpg';
+import post from '../../img/tabs/post.jpg';
+import balanced from '../../img/tabs/burger.jpg';
 import './preview.css';
 import {useState} from "react";
 const Preview = () => {
     const [menus, changeMenus] = useState([
-            {name:'Fitness' ,description:' Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Для людей, которые интересуются спортом; активных и здоровых. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', isActive: true, img: vegan},
-            {name:'Fitness2', description:'wfafw', isActive: false, img: hamburger},
-            {name:'Fitness3', description:'af', isActive: false, img: vegan},
-            {name:'Fitness4', description:'wfafw', isActive: false, img: vegan}
+            {
+                name:'Фитнес' ,description:'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Для людей, которые интересуются спортом; активных и здоровых. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+                isActive: true,
+                img: vegan
+            },
+            {
+                name:'Премиум',
+                description:'Меню “Премиум” - мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+                isActive: false,
+                img: elite
+            },
+            {
+                name:'Постное',
+                description:'Наше специальное “Постное меню” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения. Полная гармония с собой и природой в каждом элементе! Все будет Ом!',
+                isActive: false,
+                img: post
+            },
+            {
+                name:'Баланс',
+                description:'Меню "Сбалансированное" - это соответствие вашего рациона всем научным рекомендациям. Мы тщательно просчитываем вашу потребность в к/б/ж/у и создаем лучшие блюда для вас. Вкусно и очень полезно!',
+                isActive: false,
+                img: balanced
+            }
         ]);
 
     function getCurrentMenu(){
@@ -38,13 +59,11 @@ const Preview = () => {
                         </div>
                     </div>
                     <div className="tabheader">
-                        <h3></h3>
+                        <h3>Выберите стиль питания</h3>
                         <div className="tabheader__items">
                             {
-
                                  menus.map((item,key) => {
-                                     const className = item.isActive ? 'tabheader__item_active' : 'tabheader__item';
-
+                                     const className = item.isActive ? 'tabheader__item_active fade' : 'tabheader__item';
                                      return (
                                         <div key={key} className={className} onClick={() => changeMenu(key)}>{item.name}</div>
                                     );
@@ -52,6 +71,9 @@ const Preview = () => {
                             }
                         </div>
                     </div>
+                </div>
+                <div className="preview__life">
+                    Живи полной жизнью!
                 </div>
             </div>
         </div>
